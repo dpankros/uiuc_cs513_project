b8d2a2f2314d4e62496af1cde22eee882dea6751
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-13 14:36:56 -0700

    progress

--- part_2/quality_checker/src/quality_checker/__init__.py
+++ part_2/quality_checker/src/quality_checker/__init__.py
@@ -1,18 +1,31 @@
 import sqlite3
 import click
+import io
 
 from quality_checker.checks import Check
 from quality_checker.checks.table_counts import check_table_counts
 from quality_checker.checks.menu_items import check_menu_items
 
 
+def check_all(conn: sqlite3.Connection) -> str:
+    with io.StringIO() as str_io:
+        checks_to_run = ["table_counts", "menu_items"]
+        for check_name in checks_to_run:
+            title, checker = CHECK_TYPE_TO_IMPL[check_name]
+            res = checker(conn)
+            str_io.write(f"({check_name}) {title}\n{res}\n\n")
+        return str_io.getvalue()
+
+
 CHECK_TYPE_TO_IMPL: dict[str, Check] = {
+    "all": ("Run all checks", check_all),
     "table_counts": ("Listing the number of rows in each table", check_table_counts),
     "menu_items": (
         "Listing the number of broken foreign key relations in the MenuItems table",
         check_menu_items,
     ),
 }
+
 
 DB_PATH = "../../data.sqlite"
 
@@ -24,10 +37,8 @@ def main(check_type: str) -> int:
         print(f"unknown check type {check_type}")
     else:
         conn = sqlite3.Connection(DB_PATH)
-        print(f"Running {check_type} check...")
         title, checker = CHECK_TYPE_TO_IMPL[check_type]
         result = checker(conn)
-        print("\n")
-        print(title)
+        print(f"{title}\n")
         print(result)
     return 0
