585f8cbef5d633126a1ea50da7049750e7f0a0f5
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-13 16:05:13 -0700

    more dish metrics

--- part_2/cleaning-suite/src/quality_checker/checks/dish.py
+++ part_2/cleaning-suite/src/quality_checker/checks/dish.py
@@ -22,9 +22,18 @@ def check_dish(conn: sqlite3.Connection) -> str:
             "Dish.first_appeared > Dish.last_appeared",
             "select * from Dish as D where D.first_appeared > D.last_appeared",
         ),
+        (
+            "Dish.lowest_price == NULL",
+            "select * from Dish as D where D.lowest_price is NULL",
+        ),
+        (
+            "Dish.highest_price == NULL",
+            "select * from Dish as D where D.highest_price is NULL",
+        ),
     ]
     data = [
         (name, len(run_query(conn, query=query, row_factory=dish_factory)))
         for (name, query) in metrics
     ]
+
     return tabulate(tabular_data=data, headers=["Metric", "value"], tablefmt="grid")
