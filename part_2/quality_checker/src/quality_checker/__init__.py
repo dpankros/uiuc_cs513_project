8166b16cbafccce730cd26b33e8ff45a5c58b0e9
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-13 15:23:51 -0700

    all

--- part_2/quality_checker/src/quality_checker/__init__.py
+++ part_2/quality_checker/src/quality_checker/__init__.py
@@ -1,6 +1,7 @@
 import sqlite3
 import click
 import io
+from functools import cache
 
 from quality_checker.checks.menu import check_menus
 from quality_checker.checks.menu_pages import check_menu_pages
@@ -9,18 +10,7 @@ from quality_checker.checks.table_counts import check_table_counts
 from quality_checker.checks.menu_items import check_menu_items
 
 
-def check_all(conn: sqlite3.Connection) -> str:
-    with io.StringIO() as str_io:
-        checks_to_run = ["table_counts", "menu_items", "menu_pages", "menu"]
-        for check_name in checks_to_run:
-            title, checker = CHECK_TYPE_TO_IMPL[check_name]
-            res = checker(conn)
-            str_io.write(f"({check_name}) {title}\n{res}\n\n")
-        return str_io.getvalue()
-
-
 CHECK_TYPE_TO_IMPL: dict[str, Check] = {
-    "all": ("Run all checks", check_all),
     "table_counts": ("Listing the number of rows in each table", check_table_counts),
     "menu_items": (
         "Listing the numbers of broken foreign key relations in the MenuItems table",
@@ -38,17 +28,28 @@ CHECK_TYPE_TO_IMPL: dict[str, Check] = {
 
 
 DB_PATH = "../../data.sqlite"
+@cache
+def _get_conn():
+        return sqlite3.Connection(DB_PATH)
 
 
 @click.command()
 @click.option("--check-type", default="all", help="The type of check to run")
 def main(check_type: str) -> int:
-    if check_type not in CHECK_TYPE_TO_IMPL:
+    if check_type == "all":
+        checks_to_run = CHECK_TYPE_TO_IMPL.keys()
+        print(f"Running all checks ({list(checks_to_run)})")
+        with io.StringIO() as str_io:
+            for check_name in checks_to_run:
+                title, checker = CHECK_TYPE_TO_IMPL[check_name]
+                res = checker(_get_conn())
+                str_io.write(f"({check_name}) {title}\n{res}\n\n")
+            print(str_io.getvalue())
+    elif check_type not in CHECK_TYPE_TO_IMPL:
         print(f"unknown check type {check_type}")
     else:
-        conn = sqlite3.Connection(DB_PATH)
         title, checker = CHECK_TYPE_TO_IMPL[check_type]
-        result = checker(conn)
+        result = checker(_get_conn())
         print(f"{title}\n")
         print(result)
     return 0
