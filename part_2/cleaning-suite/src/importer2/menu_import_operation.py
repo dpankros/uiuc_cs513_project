9ca8fd7525a6961c53a542c19243efbb09cb7929
David Pankros <dpankros@gmail.com>
2024-07-16 17:52:43 -0400

    More column updates.  MenuImportOperation is still a WIP.

--- part_2/cleaning-suite/src/importer2/menu_import_operation.py
+++ part_2/cleaning-suite/src/importer2/menu_import_operation.py
@@ -40,19 +40,74 @@ class MenuImportOperation(OpenRefineOperation):
         #     [/;$/, ""]
         # ]
 
+        venue_expression = '.'.join([
+            'value',
+            'trim()',
+            'toLowercase()',
+            'replace(",", ";")',
+            'replace("commercial", "com")',
+            'replace("railroad", "rail")',
+            'replace("social", "soc")',
+            'replace("soc club", "soc")',
+            'replace("education", "educ")',
+            'replace("millitary", "mil")',
+            'replace("navy", "nav")',
+            'replace("polictical", "pol")',
+            'replace("government", "gov")',
+            'replace("professional", "prof")',
+            'replace("religious", "relig")',
+            'replace("musical", "mus")',
+            'replace("hotel", "hot")',
+            'replace("restaurant", "rest")',
+            'replace("foreign", "for")',
+            'replace("steamship", "steam")',
+            'replace("other (private party)", "other")', #what do we want here??
+            'replace("(?)", " ")',
+            'replace("?", " ")',
+
+
+            'replace(/\\s*;\\s*/,"; ")',  # normalize delimiter spaces
+            'replace(/\\s+/," ")',  # all spaces are at most 1
+            'trim()'
+        ])
+        venue_op = [
+            {
+                "op": "core/column-addition",
+                "engineConfig": {
+                    "facets": [],
+                    "mode": "row-based"
+                },
+                "baseColumnName": "venue",
+                "expression": f'grel:{venue_expression}',
+                "onError": "set-to-blank",
+                "newColumnName": "norm_venue",
+                "columnInsertIndex": 6,
+                "description": "Standardize delimiters and remove hard backets"
+            }
+
+        ]
+        project.apply_operations(venue_op)
+
         event_expression = '.'.join([
             'value',
             'trim()',
             'toLowercase()',
-            'replace(",", ";")', # change all comma delimiters to semicolon  BUT SEE 24787
+            'replace(",", ";")', # change all comma delimiters to semicolon  BUT SEE 24787, 13546, 13975
+            'replace(" & ", "; ")', # change all foo & bar to foo; bar
+            'replace(" and ", "; ")', # change all foo & bar to foo; bar 14159
+            'replace(" and/or ", "; ")', # change all foo and/or bar to foo; bar 13553
+            'replace("/", "; ")', # change all breakfast/supper/lunch to breakfast; supper; lunch
+            'replace("(?)", "")', # remove (?) 14076, 14078
             'replace(/;$/, "")', # remove extra hanging delimiters
             'replace(/\\(\\?(.+)\\?\\)/,"$1")', # change (something) to something
-            'replace(/\\[\\(\\](.+)[\\]\\)]\\)/,"$1")', # change ^(something)$ OR ^[something]$ to something
+            'replace(/\\[\\(\\](.+)[\\]\\)]\\)/," $1 ")', # change ^(something)$ OR ^[something]$ to something
             'replace(/([0-9]?[0-9]);([0-9][0-9])/, "$1:$2")', # change 3;00 to 3:00 (normalize times)
-            'replace(/[\\[\\]]/, "")', # remove hard brackets
-            'replace(/\\?$/,"")',  # change foo? to foo  (and all instances of just "?")
+            'replace(/[\\[\\]]/, " ")', # remove hard brackets
+            'replace(/\\?\\s*$/,"")',  # change foo? to foo  (and all instances of just "?") 14408
             'replace(/\\?;/,"")',  # change something?; something to something; something
             'replace(/^\'/,"")',  # change 'something to something
+            'replace(/\\s*;\\s*/,"; ")',  # normalize delimiter spaces, like in 13553, 13563
+            'replace(/\\s+/," ")',  # all spaces are at most 1
             'trim()'
         ])
         event_op = [
