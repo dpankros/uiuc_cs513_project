369e8aa45b0cf3e86fe90086882271beea2c604c
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-20 10:49:21 -0700

    type annotation

--- part_2/cleaning-suite/src/importer2/task/project_cleanup_task.py
+++ part_2/cleaning-suite/src/importer2/task/project_cleanup_task.py
@@ -1,12 +1,13 @@
 from .open_refine_task import OpenRefineTask
+from importer2.openrefine import Server
 
 
 class ProjectCleanupTask(OpenRefineTask):
   name = "OpenRefine Project Cleanup"
   @staticmethod
-  def delete_all_projects(server):
+  def delete_all_projects(server: Server):
     for p in server.get_all_projects():
-      server.delete_project(p.id)
+      server.delete_project(p.id)  # type: ignore
 
   def run(self):
     super().run()
