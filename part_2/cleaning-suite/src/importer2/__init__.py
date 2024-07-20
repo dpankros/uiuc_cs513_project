47b0d6632b35eca6f5847103fde4c58dfd835808
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-20 11:00:34 -0700

    cmdline and spacing

--- part_2/cleaning-suite/src/importer2/__init__.py
+++ part_2/cleaning-suite/src/importer2/__init__.py
@@ -1,3 +1,4 @@
+import click
 from sqlalchemy import create_engine
 
 from importer2.cs513_task import DishImportTask, MenuImportTask, MenuItemImportTask, MenuPageImportTask, \
@@ -7,78 +8,108 @@ from importer2.openrefine import Server, Project
 from importer2.task import TaskList, ProjectCleanupTask
 
 
-def main():
+@click.command()
+@click.option(
+    "--run-imports",
+    default=False,
+    help="Whether to re-import data to SQLite",
+)
+@click.option(
+    "--run-verifications",
+    default=False,
+    help="Whether to run verification on cleaned data",
+)
+@click.option(
+    "--run-reports",
+    default=False,
+    help="Whether to run data reports"
+)
+def main(run_imports: bool, run_verifications: bool, run_reports: bool):
     base_config = {
         'server_url': "http://localhost:3333",
         'sql_engine': create_engine('sqlite:///../../export.db'),
         "max_count": 10,
     }
 
-    TaskList([
-        #
-        # Verbatim imports - Make a copy of the data in sqlite without ANY modification
-        #
-
-        # VerbatimImportTask({
-        #   **base_config,
-        #   'source_filename': '../../data/Dish.csv',
-        #   'sql_table': 'dish_orig',
-        #   'sql_if_exists': 'replace',
-        #   'name': 'Import dish_orig unmodified data',
-        # }),
-        # VerbatimImportTask({
-        #   **base_config,
-        #   'source_filename': '../../data/Menu.csv',
-        #   'sql_table': 'menu_orig',
-        #   'sql_if_exists': 'replace',
-        #   'name': 'Import menu_orig unmodified data',
-        # }),
-        # VerbatimImportTask({
-        #   **base_config,
-        #   'source_filename': '../../data/MenuItem.csv',
-        #   'sql_table': 'menu_item_orig',
-        #   'sql_if_exists': 'replace',
-        #   'name': 'Import menu_item_orig unmodified data',
-        # }),
-        # VerbatimImportTask({
-        #   **base_config,
-        #   'source_filename': '../../data/MenuPage.csv',
-        #   'sql_table': 'menu_page_orig',
-        #   'sql_if_exists': 'replace',
-        #   'name': 'Import menu_page_orig unmodified data',
-        # }),
-        #
-        # #
-        # # Cleaned imports - These read the CSV and clean the data prior to import
-        # #
-        #
-        DishImportTask({
+    # Verbatim imports - Make a copy of the data in sqlite without ANY modification
+    imports = [
+        VerbatimImportTask({
           **base_config,
           'source_filename': '../../data/Dish.csv',
-          'dest_filename': '../../Dish_conv.csv',
-          'sql_table': 'dish',
-          'sql_if_exists': 'replace'
+          'sql_table': 'dish_orig',
+          'sql_if_exists': 'replace',
+          'name': 'Import dish_orig unmodified data',
         }),
-        MenuImportTask({
+        VerbatimImportTask({
           **base_config,
           'source_filename': '../../data/Menu.csv',
-          'dest_filename': '../../Menu_conv.csv',
-          'sql_table': 'menu',
-          'sql_if_exists': 'replace'
+          'sql_table': 'menu_orig',
+          'sql_if_exists': 'replace',
+          'name': 'Import menu_orig unmodified data',
         }),
-        MenuItemImportTask({
+        VerbatimImportTask({
           **base_config,
           'source_filename': '../../data/MenuItem.csv',
-          'dest_filename': '../../MenuItem_conv.csv',
-          'sql_table': 'menu_item',
-          'sql_if_exists': 'replace'
+          'sql_table': 'menu_item_orig',
+          'sql_if_exists': 'replace',
+          'name': 'Import menu_item_orig unmodified data',
         }),
-        MenuPageImportTask({
+        VerbatimImportTask({
           **base_config,
           'source_filename': '../../data/MenuPage.csv',
-          'dest_filename': '../../MenuPage_conv.csv',
-          'sql_table': 'menu_page',
-          'sql_if_exists': 'replace'
+          'sql_table': 'menu_page_orig',
+          'sql_if_exists': 'replace',
+          'name': 'Import menu_page_orig unmodified data',
+        }),
+    ] if run_imports else []
+
+    # Verification tasks - these judge whether the data is an exact match so it's not very good
+    verifications = [
+        DishVerificationTask(base_config),
+        MenuVerificationTask(base_config),
+        MenuPageVerificationTask(base_config),
+        MenuItemVerificationTask(base_config),
+    ] if run_verifications else []
+
+    # Reporting tasks - these provide data outputs.
+    # TODO: These should show the range of
+    reports = [
+        MenuItemReportTask({**base_config, 'table': 'menu_item_orig', 'name': 'Original Menu Item IC Violations Task'}),
+        MenuItemReportTask({**base_config, 'table': 'menu_item', 'name': 'Updated Menu Item IC Violations Task'}),
+        MenuPageReportTask({**base_config, 'table': 'menu_page_orig', 'name': 'Original Menu Page IC Violations Task'}),
+        MenuPageReportTask({**base_config, 'table': 'menu_page', 'name': 'Update Menu Item IC Violations Task'}),
+    ] if run_reports else []
+
+    TaskList([
+        *imports,
+        # Cleaned imports - These read the CSV and clean the data prior to import
+        DishImportTask({
+            **base_config,
+            'source_filename': '../../data/Dish.csv',
+            'dest_filename': '../../Dish_conv.csv',
+            'sql_table': 'dish',
+            'sql_if_exists': 'replace'
+        }),
+        MenuImportTask({
+            **base_config,
+            'source_filename': '../../data/Menu.csv',
+            'dest_filename': '../../Menu_conv.csv',
+            'sql_table': 'menu',
+            'sql_if_exists': 'replace'
+        }),
+        MenuItemImportTask({
+            **base_config,
+            'source_filename': '../../data/MenuItem.csv',
+            'dest_filename': '../../MenuItem_conv.csv',
+            'sql_table': 'menu_item',
+            'sql_if_exists': 'replace'
+        }),
+        MenuPageImportTask({
+            **base_config,
+            'source_filename': '../../data/MenuPage.csv',
+            'dest_filename': '../../MenuPage_conv.csv',
+            'sql_table': 'menu_page',
+            'sql_if_exists': 'replace'
         }),
         # # keep after all the Import tasks as it deletes all the projects from openrefine
         ProjectCleanupTask(base_config),
@@ -125,22 +156,8 @@ def main():
             'name': 'MenuPage Cleaned Stats',
         }),
 
-        # Verification tasks - these judge whether the data is an exact match so it's not very good
-        #
-        # DishVerificationTask(base_config),
-        # MenuVerificationTask(base_config),
-        # MenuPageVerificationTask(base_config),
-        # MenuItemVerificationTask(base_config),
-
-        #
-        # Reporting tasks - these provide data outputs.
-        # TODO: These should show the range of
-        #
-        # MenuItemReportTask({**base_config, 'table': 'menu_item_orig', 'name': 'Original Menu Item IC Violations Task'}),
-        # MenuItemReportTask({**base_config, 'table': 'menu_item', 'name': 'Updated Menu Item IC Violations Task'}),
-        # MenuPageReportTask({**base_config, 'table': 'menu_page_orig', 'name': 'Original Menu Page IC Violations Task'}),
-        # MenuPageReportTask({**base_config, 'table': 'menu_page', 'name': 'Update Menu Item IC Violations Task'}),
-
+        *verifications,
+        *reports,
     ]).run()
 
 
