35c115309b3292d5a47169808e95d3d874a1069c
Aaron Schlesinger <aaron@ecomaz.net>
2024-07-21 11:35:57 -0700

    tiny typo fix

--- part_2/cleaning-suite/src/importer2/cs513_task/cs513_report_task.py
+++ part_2/cleaning-suite/src/importer2/cs513_task/cs513_report_task.py
@@ -169,7 +169,7 @@ ORDER BY count desc;
                             constraint = self.integrity_constraints[col]
                             violations = constraint.check(
                                 col,  # column name
-                                getattr(row, col),  # colum value
+                                getattr(row, col),  # column value
                                 {  # context object
                                     "row": row,
                                     "table_name": self.comparison_table,
@@ -184,7 +184,7 @@ ORDER BY count desc;
 
                 GROUPS = ['count', 'min', 'max', 'avg']
                 print(f"  Column Stats for {self.comparison_table} compared to {self.base_table}")
-                group_headers = ''.join([f"{g.title():>5} Before {g.title():>5} After  {g.title():>5} Impr" for g in GROUPS])
+                group_headers = ''.join([f"{g.title():>5} Beforeeee {g.title():>5} After  {g.title():>5} Impr" for g in GROUPS])
                 print(f"                  Column {group_headers}")
 
                 for before_col, stat_obj in self.stat_columns.items():
