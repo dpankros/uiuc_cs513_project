216eeb1b4c9ad7f1adaade0506420191e6f6799e
David Pankros <dpankros@gmail.com>
2024-07-22 09:09:40 -0400

    Change build.sh to better handle tables

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 FORMAT="gfm"
-
+FROM="markdown+grid_tables+backtick_code_blocks+multiline_tables"
 mkdir -p 'build'
 
 for f in *.md; do
   filename="$(basename "$f")"
   base="${filename%.*}"
   ext="${filename##*.}"
-  pandoc "$f" --template=eisvogel -F mermaid-filter -f $FORMAT -o "build/${base}.pdf"
+  pandoc "$f" --template=eisvogel -F mermaid-filter -f $FORMAT --from "$FROM" -o "build/${base}.pdf"
 done
